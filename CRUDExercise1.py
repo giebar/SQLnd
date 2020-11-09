0fceebfeb8089353ee31bb76f6a49a52d1100ac5
Giedre <giedre1996@gmail.com>
2020-11-09 12:10:17 +0200

    second commit

--- CRUDExercise1.py
+++ CRUDExercise1.py
@@ -5,67 +5,59 @@ from database import DatabaseContextManager
 # Tables = "Customer"
 # fields = [first_name, last_name, address, age]
 
-def create_table_customer():
-    query = """CREATE TABLE IF NOT EXISTS Customer(
- id INTEGER PRIMARY KEY AUTOINCREMENT,
- first_name TEXT,
- last_name TEXT,
- address TEXT,
- age INTEGER)"""
-
-
-with DatabaseContextManager("CRUD") as db:
-    db.execute(query)
+def create_table_customers():
+    query = """CREATE TABLE IF NOT EXISTS Customers(
+             id INTEGER PRIMARY KEY AUTOINCREMENT,
+             first_name TEXT,
+             last_name TEXT,
+             address TEXT,
+             age INTEGER)"""
+    with DatabaseContextManager("CRUD") as db:
+        db.execute(query)
 
 
 # Create function
-def create_customer(first_name: str, last_name: str, addres: str, age: int):
-    query = """INSERT INTO Customer(first_name, last_name, address, age) VALUES(?,?,?,?)"""
-
-
-parameters = [first_name, last_name, address, age]
-with DatabaseContextManager("CRUD") as db:
-    db.execute(query, parameters)
+def create_customer(first_name: str, last_name: str, address: str, age: int):
+    query = """INSERT INTO Customers(first_name, last_name, address, age) VALUES(?,?,?,?)"""
+    # Question marks are used in initial query to have placeholders for upcoming parameters.
+    # (This is used to protect ourselves from SQL Injection attacks)
+    parameters = [first_name, last_name, address, age]
+    # Parameters are used to pass values that were given when calling the function.
+    with DatabaseContextManager("CRUD") as db:
+        db.execute(query, parameters)
 
 
 # Read function
 def get_customer():
-    query = """SELECT * FROM Customer"""
-
-
-with DatabaseContextManager("CRUD") as db:
-    db.execute(query)
-for record in db.fetchall():
-    print(record)
-print("------------------------------------------------------")
-
-
-# print for convenience in terminal
+    query = """SELECT * FROM Customers"""
+    with DatabaseContextManager("CRUD") as db:
+        db.execute(query)
+        for record in db.fetchall():
+            print(record)
 
 
 # Update function
-def update_customer(first_name, last_name, age):
-    query = """UPDATE Customer
- SET age = ?
- WHERE first_name = ? AND last_name = ?"""
-
-
-parameters = [age, first_name, last_name]
-with DatabaseContextManager("CRUD") as db:
-    db.execute(query, parameters)
+def update_customer(age: int, first_name: str, last_name: str):
+    query = """UPDATE Customers
+                SET age = ?
+                WHERE first_name = ? AND last_name = ?"""
+    parameters = [age, first_name, last_name]
+    with DatabaseContextManager("CRUD") as db:
+        db.execute(query, parameters)
 
 
 # Delete function
-def delete_customer(last_name):
-    query = """DELETE FROM Customer
- WHERE last_name = ?"""
+def delete_customer(last_name: str):
+    query = """DELETE FROM Customers
+                WHERE last_name = ?"""
+    parameters = [last_name]
+    with DatabaseContextManager("CRUD") as db:
+        db.execute(query, parameters)
 
 
-parameters = [last_name]
-with DatabaseContextManager("CRUD") as db:
-    db.execute(query, parameters)
-
-create_table_customer()
-create_customer("Jonas", "Jonaitis", "Kaunas", 30)
-update_customer("Jonas", "Jonaitis", 20)
+create_table_customers()
+create_customer('John', 'Johnson', 'Kaunas', 25)
+update_customer(26, 'John', 'Johnson')
+delete_customer('Johnson')
 get_customer()
+
